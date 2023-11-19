763738144c0e806967654295333d3900436a06e9
zidankhan86 <zidankhan718@gmail.com>
2023-11-19 23:02:27 +0600

    up todate

--- resources/views/frontend/pages/socialShare.blade.php
+++ resources/views/frontend/pages/socialShare.blade.php
@@ -31,7 +31,7 @@
     </head>
     <body>
         <div class="container mt-4">
-            <h2 class="mb-5 text-center">Laravel Social Share Buttons Example</h2>
+            <h2 class="mb-5 text-center">Thanks for stay with us</h2>
             {!! $shareComponent !!}
         </div>
     </body>
